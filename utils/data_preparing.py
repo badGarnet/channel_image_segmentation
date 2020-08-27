2147d06244d220199cdfc3ae86bdc0ec8c7e613b
Yao You <theyaoyou@gmail.com>
2020-08-27 13:44:57 -0400

    add comments

--- utils/data_preparing.py
+++ utils/data_preparing.py
@@ -45,7 +45,18 @@ def load_multi_channel_data(path, extension, features=None, masks=None, process=
         return data[:, :, :, features], data[:, :, :, masks]
 
 
-def crop_image(image, idx=0, img_height=500, img_width=300, img_channel=5):
+def crop_image(image, img_height=500, img_width=300, img_channel=5):
+    """cropping image using tfa's random_crop method
+
+    Args:
+        image (Tensor): of shape (n_batch, height, width, img_channel)
+        img_height (int, optional): height of crop area in pixels. Defaults to 500.
+        img_width (int, optional): width of crop area in pixels. Defaults to 300.
+        img_channel (int, optional): number of channels in the image. Defaults to 5.
+
+    Returns:
+        Tensor: cropped Tensor of shape (n_batch, img_height, img_width, img_channel)
+    """
     cropped = tf.image.random_crop(
         image, [img_height, img_width, img_channel], 
         seed=None, name=None
@@ -54,6 +65,18 @@ def crop_image(image, idx=0, img_height=500, img_width=300, img_channel=5):
 
 
 def save_crops(tensor, prefix='', batch=0, idx=0, path=Path('.')):
+    """save tensor into images of RGB, elevation (4th channel) and mask (5th channel)
+    make sure the input images are configured in such a way
+
+    TODO: make this more flexible for more types of channels (e.g., slope)
+
+    Args:
+        tensor (Tensor): must have last dim's size equal to 5
+        prefix (str, optional): prefix of the file. Defaults to ''.
+        batch (int, optional): batch number. Defaults to 0.
+        idx (int, optional): image number. Defaults to 0.
+        path ([type], optional): path to save the files. Defaults to Path('.').
+    """
     prefix += f'batch_{batch}_num_{idx}_'
     tf.keras.preprocessing.image.save_img(
         path / (prefix + 'image.png'), tensor[:, :, :3]
@@ -81,7 +104,8 @@ def main():
         n_pass = 2
         seeds = np.random.randint(0, 1000, n_pass)
         for i_pass in range(n_pass):
-            print(f'cropping pass {i_pass+1}')
+            print(f'cropping pass {i_pass+1} of dataset {part}...', end='')
+            # map cropping method onto the dataset
             cropped_slices = (
             dslice
                 .shuffle(seeds[i_pass])
@@ -89,7 +113,7 @@ def main():
                 .batch(10)
                 .prefetch(tf.data.experimental.AUTOTUNE)
             )
-            print('saveing data')
+            print('saving data')
             for i, batch in enumerate(cropped_slices):
                 for j, image in enumerate(batch):
                     save_crops(image, prefix=f'part_{part}_', batch=i, idx=j, path=data_path / 'crops')
